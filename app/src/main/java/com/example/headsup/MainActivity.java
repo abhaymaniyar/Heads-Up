0565e31daa4db9c493f446c1deb268c4eca22ff9
abhaymaniyar <abhaymaniyar@live.in>
2017-06-10 02:12:09 +0530

    [Added] text files for other categories and removed unncecessary hard coded strings.

--- app/src/main/java/com/example/headsup/MainActivity.java
+++ app/src/main/java/com/example/headsup/MainActivity.java
@@ -117,7 +117,6 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.album2,
                 R.drawable.album3,
                 R.drawable.album4,
-                R.drawable.album5,
                 R.drawable.album6,
                 R.drawable.album7,
                 R.drawable.album8
@@ -127,112 +126,59 @@ public class MainActivity extends AppCompatActivity {
         categoriesList.add(new Categories("Songs", 15, covers[1]));
         categoriesList.add(new Categories("Act it out", 15, covers[2]));
         categoriesList.add(new Categories("Game of Thrones", 15, covers[3]));
-        categoriesList.add(new Categories("Actors", 15, covers[4]));
-        categoriesList.add(new Categories("Green Earth", 15, covers[5]));
-        categoriesList.add(new Categories("Dialogues", 15, covers[6]));
+        categoriesList.add(new Categories("Actors", 15, covers[6]));
+        categoriesList.add(new Categories("Green Earth", 15, covers[4]));
+        categoriesList.add(new Categories("Dialogues", 15, covers[5]));
 
         adapter.notifyDataSetChanged();
 
     }
 
     private void prepareStrings() throws IOException {
-        InputStreamReader streamReader = new InputStreamReader(getAssets().open("movies.txt"));
-        BufferedReader bufferedReader = new BufferedReader(streamReader);
+        ArrayList<String> moviesString = new ArrayList<>(35);
+        ArrayList<String> songsString = new ArrayList<>(24);
+        ArrayList<String> actItOutString = new ArrayList<>(15);
+        ArrayList<String> gotString = new ArrayList<>(15);
+
         int ch;
         String str = "";
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("movies.txt")));
         while((ch = bufferedReader.read()) != -1){
             str += (char)ch;
         }
 
         String[] strArray = str.split(",");
-        ArrayList<String> moviesString = new ArrayList<>(35);
         for (int i = 0; i < strArray.length; i++){
             moviesString.add(strArray[i]);
         }
-//        moviesString.add("Kabhi Khushi kabhi bum");
-//        moviesString.add("Dil Chahta hai");
-//        moviesString.add("Hero");
-//        moviesString.add("Raja Hindustani");
-//        moviesString.add("Ram Lakhan");
-//        moviesString.add("Fast and Furious");
-//        moviesString.add("Hello Brother");
-//        moviesString.add("Rang de Basanti");
-//        moviesString.add("Bajarangi Bhaijan");
-//        moviesString.add("Gangs of Wasseypur");
-//        moviesString.add("Dus");
-//        moviesString.add("Sholay");
-//        moviesString.add("Kaho na pyaar he");
-//        moviesString.add("Koi Mil gaya");
-//        moviesString.add("Dharam Veer");
-//        moviesString.add("Karan Arjun");
-//        moviesString.add("Mere Badle ki Aag");
-//        moviesString.add("Aan - Man at Work");
-//        moviesString.add("Pardes");
-//        moviesString.add("Jis desh me Ganga rehta he");
-//        moviesString.add("Baahubali - The conclusion");
-//        moviesString.add("Mother India");
-//        moviesString.add("Koora Kagaj");
-//        moviesString.add("Jharokha");
-//        moviesString.add("Shola aur Shabnam");
-//        moviesString.add("Koyla");
-//        moviesString.add("Krrish");
-//        moviesString.add("Dhamaal");
-//        moviesString.add("Kashmir ki Kali");
-//        moviesString.add("Main hoon na!");
-//        moviesString.add("Andaz apna apna");
-//        moviesString.add("Padosan");
-//        moviesString.add("Jabaaz");
-//        moviesString.add("Befikre");
-//        moviesString.add("Mr. India");
-//        moviesString.add("Kaypo Che");
-//        moviesString.add("Janwar");
         stringsList.put(categoriesList.get(0).getName(), moviesString);
-        ArrayList<String> songsString = new ArrayList<>(24);
-        songsString.add("Koi tumsa nahi");
-        songsString.add("Tera chehra jab nazar");
-        songsString.add("Na na na re");
-        songsString.add("Dil Ibadat");
-        songsString.add("Mohabattein");
-        songsString.add("Ek pal ka jeena");
-        songsString.add("Kaho na pyaar he");
-        songsString.add("Kyu Chalti he pawan");
-        songsString.add("Juli Juli");
-        songsString.add("Jimmy Jimmy aaj aaja");
-        songsString.add("Me nikla gaddi leke");
-        songsString.add("Musafir jane wale");
-        songsString.add("Koi mujhe jungli kahe");
-        songsString.add("Mohabbat barsa dena tu");
-        songsString.add("Sawan me lag gayi aag");
-        songsString.add("Tunak Tunak tun tarara");
-        songsString.add("Satyam Shivam Sundaram");
-        songsString.add("Baharo full barsao");
-        songsString.add("Barso re megha megha");
-        songsString.add("Ye jo des he tera");
-        songsString.add("I love my India");
-        songsString.add("Ab tumhare hawale watan sathiyo");
-        songsString.add("Hamma hamma");
-        songsString.add("Jumma Chumma dede");
+
+        str = "";
+        bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("songs.txt")));
+        while((ch = bufferedReader.read()) != -1){
+            str += (char)ch;
+        }
+
+        strArray = str.split(",");
+
+        for (int i = 0; i < strArray.length; i++){
+            songsString.add(strArray[i]);
+        }
         stringsList.put(categoriesList.get(1).getName(), songsString);
-        ArrayList<String> actItOutString = new ArrayList<>(15);
-        actItOutString.add("Sky Diving");
-        actItOutString.add("Washing Clothes");
-        actItOutString.add("Flying Kite");
-        actItOutString.add("Making a Goal");
-        actItOutString.add("Breaking the door and entering");
-        actItOutString.add("Making a cup of tea");
-        actItOutString.add("Playing Cricket");
-        actItOutString.add("Smoking Weed");
-        actItOutString.add("Teaching a class");
-        actItOutString.add("Doing pushups");
-        actItOutString.add("Serving Pani puri");
-        actItOutString.add("Laughing at you");
-        actItOutString.add("1Playing Cricket");
-        actItOutString.add("2Smoking Weed");
-        actItOutString.add("3Teaching a class");
-        actItOutString.add("4Doing pushups");
-        actItOutString.add("5Serving Pani puri");
-        actItOutString.add("6Laughing at you");
+
+        str = "";
+        bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("act it out.txt")));
+        while((ch = bufferedReader.read()) != -1){
+            str += (char)ch;
+        }
+
+        strArray = str.split(",");
+
+        for (int i = 0; i < strArray.length; i++){
+            actItOutString.add(strArray[i]);
+        }
         stringsList.put(categoriesList.get(2).getName(), actItOutString);
+
         ArrayList<String> actorString = new ArrayList<>(25);
         actorString.add("Shatrugun Sinha");
         actorString.add("Rajpal Yadav");
@@ -290,19 +236,18 @@ public class MainActivity extends AppCompatActivity {
         dialoguesString.add("2111Teri Chahta hai");
         dialoguesString.add("111Teri Chahta hai");
         stringsList.put(categoriesList.get(5).getName(), dialoguesString);
-        ArrayList<String> gotString = new ArrayList<>(15);
-        gotString.add("Naino Khushi kabhi bum");
-        gotString.add("Teri Chahta hai");
-        gotString.add("2Naino Khushi kabhi bum");
-        gotString.add("1Teri Chahta hai");
-        gotString.add("12Naino Khushi kabhi bum");
-        gotString.add("21Teri Chahta hai");
-        gotString.add("4Naino Khushi kabhi bum");
-        gotString.add("32Teri Chahta hai");
-        gotString.add("22Teri Chahta hai");
-        gotString.add("5Naino Khushi kabhi bum");
-        gotString.add("2111Teri Chahta hai");
-        gotString.add("111Teri Chahta hai");
+
+        str = "";
+        bufferedReader = new BufferedReader(new InputStreamReader(getAssets().open("game of thrones.txt")));
+        while((ch = bufferedReader.read()) != -1){
+            str += (char)ch;
+        }
+
+        strArray = str.split(",");
+
+        for (int i = 0; i < strArray.length; i++){
+            gotString.add(strArray[i]);
+        }
         stringsList.put(categoriesList.get(6).getName(), gotString);
         adapter.notifyDataSetChanged();
     }
