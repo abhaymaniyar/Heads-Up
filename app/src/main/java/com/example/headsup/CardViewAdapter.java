67f15c2cb908a0b38297d5590feb2cfc3c8960f2
abhaymaniyar <abhaymaniyar@live.in>
2017-06-21 12:33:26 +0530

    [Fixed] No delay in extiting game

--- app/src/main/java/com/example/headsup/CardViewAdapter.java
+++ app/src/main/java/com/example/headsup/CardViewAdapter.java
@@ -3,13 +3,18 @@ package com.example.headsup;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
